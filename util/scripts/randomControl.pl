a7a705a4e76652afc553f3afd714094c70d97358
Cluster3Admin <admin@ODay.local>
2014-12-13 22:44:23 -0800

    Uploaded more automation scripts

---
+++ util/scripts/randomControl.pl
@@ -0,0 +1,61 @@
+use Data::Dumper;
+
+
+my %randpar;
+my %couepar;
+my %recupar;
+my @dirList;
+open(FRA, "randpar.param") or die $!;
+open(FCO, "couepar.param") or die $!;
+open(FRE, "recupar.param") or die $!;
+while(<FRA>) {
+    chomp;
+    my ($key,$value) = split(' ',$_);
+    $randpar{$key} = $value;
+}
+
+while(<FCO>) {
+    chomp;
+    my ($key,$value) = split(' ',$_);
+    $couepar{$key} = $value;
+}
+
+while(<FRE>) {
+    chomp;
+    my ($key,$value) = split(' ',$_);
+    $recupar{$key} = $value;
+}
+close(FRA);
+close(FCO);
+close(FRE);
+my $directory = './';
+opendir(DIR, $directory) or die $!;
+while(my $subdir = readdir(DIR)){
+    if($subdir =~ /random(\d)+/){
+	push(@dirList,$subdir);
+    }
+}
+@commands;
+my $randparstr = join(" ",map { "$_ $randpar{$_}" } keys %randpar);
+my $coueparstr = join(" ",map { "$_ $couepar{$_}" } keys %couepar);
+my $recuparstr = join(" ",map { "$_ $recupar{$_}" } keys %recupar);
+
+
+foreach $randDir (@dirList){
+    my $pid;
+    next if $pid = fork;
+    die "fork failed: $!" unless defined $pid;
+    system("mkdir $randDir/log");
+    system("randomfield $randparstr $randDir/random > $randDir/log/rand.log");
+    system("couette $coueparstr -o $randDir/data -l2 $randDir/random > $randDir/log/couette.log");
+    system("recurrence.x $recuparstr -d $randDir/data $randDir/rec > $randDir/log/rec.log");
+    system("git add $randDir/rec.csv");
+    exit;
+    
+}
+1 while (wait() != -1);
+
+print "All done!\n";
+print "$randparstr\n";
+print "$coueparstr\n";
+print "$recuparstr\n";
