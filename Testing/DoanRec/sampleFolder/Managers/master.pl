367cf50d26113e6aa15e2cc4d5614955cab7f1e8
Cluster2Admin <admin@Doan.local>
2014-12-15 16:03:41 -0800

    Restructuring perl workflow 12/15

---
+++ Testing/DoanRec/sampleFolder/Managers/master.pl
@@ -0,0 +1,6 @@
+print("Randomizing\n");
+system("perl randGen.pl");
+print("Integrating\n");
+system("perl coueGen.pl");
+system("Recurring\n");
+system("perl recuGen.pl");
--- util/scripts/randomControl.pl
+++ util/scripts/randomControl.pl
@@ -1,33 +1,5 @@
-use Data::Dumper;
+@dirList;
 
-
-my %randpar;
-my %couepar;
-my %recupar;
-my @dirList;
-open(FRA, "randpar.param") or die $!;
-open(FCO, "couepar.param") or die $!;
-open(FRE, "recupar.param") or die $!;
-while(<FRA>) {
-    chomp;
-    my ($key,$value) = split(' ',$_);
-    $randpar{$key} = $value;
-}
-
-while(<FCO>) {
-    chomp;
-    my ($key,$value) = split(' ',$_);
-    $couepar{$key} = $value;
-}
-
-while(<FRE>) {
-    chomp;
-    my ($key,$value) = split(' ',$_);
-    $recupar{$key} = $value;
-}
-close(FRA);
-close(FCO);
-close(FRE);
 my $directory = './';
 opendir(DIR, $directory) or die $!;
 while(my $subdir = readdir(DIR)){
@@ -35,32 +7,14 @@ while(my $subdir = readdir(DIR)){
 	push(@dirList,$subdir);
     }
 }
-@commands;
-my $randparstr = join(" ",map { "$_ $randpar{$_}" } keys %randpar);
-my $coueparstr = join(" ",map { "$_ $couepar{$_}" } keys %couepar);
-my $recuparstr = join(" ",map { "$_ $recupar{$_}" } keys %recupar);
-
 
 foreach $randDir (@dirList){
     next if $pid = fork;
     die "fork failed: $!" unless defined $pid;
-    my @array = split('m',$randDir);
-    srand();
-    my $seed = int(rand(3000000000));
-    system("mkdir $randDir/log");
-    my $randExec = "randomfield $randparstr -sd $seed $randDir/random.ff > $randDir/log/rand.log";
-    print "$randExec\n";
-    system($randExec);
-    my $coueExec = "couette $coueparstr -o $randDir/data -l2 $randDir/random.ff > $randDir/log/couette.log";
-    print "$coueExec\n";
-    system($coueExec);
-    system("recurrence.x $recuparstr -d $randDir/data $randDir/rec > $randDir/log/rec.log");
+    print "Beginning in $randDir\n";
+    system("perl $randDir/Managers/master.pl");
     exit;
     
 }
 1 while (wait() != -1);
 
-print "All done!\n";
-print "$randparstr\n";
-print "$coueparstr\n";
-print "$recuparstr\n";
